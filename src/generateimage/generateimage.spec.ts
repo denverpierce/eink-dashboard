a45e3560781b2f2e9af54a2e0621e772d89dce28
denverpierce <denver.pierce@gmail.com>
2023-02-12 12:26:17 -0600

    Fetch CDP server inside the test

--- src/generateimage/generateimage.spec.ts
+++ src/generateimage/generateimage.spec.ts
@@ -1,18 +1,27 @@
 import { test, expect, chromium } from '@playwright/test';
 
 test('Take Screenshot', async () => {
-console.log(process.env.CDP_URL);
-const browser = await chromium.connectOverCDP(process.env.CDP_URL);
-const defaultContext = browser.contexts()[0];
-const page = defaultContext.pages()[0];  
-await page.goto('http://localhost:3000/');
+  const chromiumCDPCheck = await (await fetch('https://localhost:3333/json/version')).json();
+  if (!('webSocketDebuggerUrl' in chromiumCDPCheck ||
+    !(typeof chromiumCDPCheck.webSocketDebuggerUrl !== 'string'))) {
+    console.error("Couldn't Chrome Devtool Server, exiting");
+    process.exit();
+  }
+
+  const cdpUrl = chromiumCDPCheck.webSocketDebuggerUrl;
+  const browser = await chromium.connectOverCDP(cdpUrl);
+  const defaultContext = browser.contexts()[0];
+  const page = defaultContext.pages()[0];
+
+  await page.goto('http://localhost:3000/');
   await page.waitForSelector('.day-forecast-day');
   await page.waitForSelector('.highcharts-container ');
   const container = await page.$('.container');
+
   if (container) {
-     await container.screenshot({
-       path: 'output/generated_image.png',
-       type: 'png'
-     })
+    await container.screenshot({
+      path: 'output/generated_image.png',
+      type: 'png'
+    })
   }
 });
--- startCDP.sh
+++ startCDP.sh
@@ -1,8 +1,6 @@
+#!/usr/bin/env bash
+
+set -xeu
+set -o pipefail
+
 /usr/lib/chromium/chromium ----remote-debugging-address=localhost/cdp --remote-debugging-port=3333 --headless
-
-
-#export CDP_URL=$(curl --silent localhost:3333/json/version | jq -r .webSocketDebuggerUrl) &
-
-#npm start &
-
-#npm run generateImage
