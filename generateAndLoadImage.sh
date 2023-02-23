f2c2a1846b493ad2f89aee34343fdfb01900c3c9
Denver Pierce <denver.pierce@gmail.com>
2023-02-22 22:56:56 -0600

    Adjust run script to work in cron

--- generateAndLoadImage.sh
+++ generateAndLoadImage.sh
@@ -4,6 +4,10 @@ set -xeu
 set -o pipefail
 
 export PYTHONPATH='/home/dash/.nvm/versions/node/v18.14.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/home/dash/12.48inch-e-paper/RaspberryPi/python/lib'
+export PATH='/home/dash/.nvm/versions/node/v18.14.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games'
+
+cd /home/dash/eink-dashboard
 
 npm run generateImage
-python3 /home/dash/waveshare-12.48inch-3-color-e-ink/display.py -i /home/dash/eink-dashboard/output/generated_image.png
+cd /home/dash/waveshare-12.48inch-3-color-e-ink/
+/usr/bin/python3 display.py -i /home/dash/eink-dashboard/output/generated_image.png
