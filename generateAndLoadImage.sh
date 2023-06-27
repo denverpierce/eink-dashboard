c0dff7736dec866eade694c053f49eac14c02e85
Denver Pierce <denver.pierce@gmail.com>
2023-06-26 19:21:12 -0500

    Restore hardcoded paths

--- generateAndLoadImage.sh
+++ generateAndLoadImage.sh
@@ -3,8 +3,9 @@
 set -xeu
 set -o pipefail
 
-export PYTHONPATH='/home/dash/.nvm/versions/node/v18.14.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/home/dash/12.48inch-e-paper/RaspberryPi/python/lib'
-export PATH=/home/dash/.nvm/versions/node/v18.16.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+export PYTHONPATH='/home/dash/waveshare-12.48inch-3-color-e-ink:/home/dash/12.48inch-e-paper/RaspberryPi/python/lib'
+export PATH='/home/dash/.nvm/versions/node/v18.16.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games'
+
 
 cd /home/dash/eink-dashboard
 
